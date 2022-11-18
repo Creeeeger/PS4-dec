c7f19d5a4cc8029e2b18dfa889dc9f5a77f09538
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2022-11-18 08:39:57 +0000

    feat: add jailbreak and runtime patches

--- libPS4/source/payload_utils.c
+++ libPS4/source/payload_utils.c
@@ -87,3 +87,253 @@ int kpayload_dump(struct thread *td, struct kpayload_dump_args *args) {
   return ret;
 }
 
+int kpayload_jailbreak(struct thread *td, struct kpayload_firmware_args *args) {
+  struct filedesc *fd;
+  struct ucred *cred;
+  fd = td->td_proc->p_fd;
+  cred = td->td_proc->p_ucred;
+
+  void *kernel_base;
+  uint8_t *kernel_ptr;
+  void **prison0;
+  void **rootvnode;
+
+  uint16_t fw_version = args->kpayload_firmware_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, jailbreak_macro);
+
+  cred->cr_uid = 0;
+  cred->cr_ruid = 0;
+  cred->cr_rgid = 0;
+  cred->cr_groups[0] = 0;
+
+  cred->cr_prison = *prison0;
+  fd->fd_rdir = fd->fd_jdir = *rootvnode;
+
+  void *td_ucred = *(void **)(((char *)td) + 304);
+
+  uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
+  *sonyCred = 0xffffffffffffffff;
+
+  uint64_t *sceProcessAuthorityId = (uint64_t *)(((char *)td_ucred) + 88);
+  *sceProcessAuthorityId = 0x3801000000000013;
+
+  uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
+  *sceProcCap = 0xffffffffffffffff;
+
+  return 0;
+}
+
+int kpayload_mmap(struct thread *td, struct kpayload_firmware_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+  uint8_t *kernel_ptr;
+
+  uint8_t *kmem;
+  uint8_t *mmap_patch_1;
+  uint8_t *mmap_patch_2;
+  uint8_t *mmap_patch_3;
+
+  uint16_t fw_version = args->kpayload_firmware_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, mmap_macro);
+
+  uint64_t cr0 = readCr0();
+  writeCr0(cr0 & ~X86_CR0_WP);
+
+  kmem = (uint8_t *)mmap_patch_1;
+  kmem[0] = 0xB8;
+  kmem[1] = 0x01;
+  kmem[2] = 0x00;
+  kmem[3] = 0x00;
+  kmem[4] = 0x00;
+  kmem[5] = 0xC3;
+
+  kmem = (uint8_t *)mmap_patch_2;
+  kmem[0] = 0xB8;
+  kmem[1] = 0x01;
+  kmem[2] = 0x00;
+  kmem[3] = 0x00;
+  kmem[4] = 0x00;
+  kmem[5] = 0xC3;
+
+  kmem = (uint8_t *)mmap_patch_3;
+  kmem[0] = 0x31;
+  kmem[1] = 0xC0;
+  kmem[2] = 0xEB;
+  kmem[3] = 0x01;
+
+  writeCr0(cr0);
+
+  return 0;
+}
+
+int kpayload_aslr(struct thread *td, struct kpayload_firmware_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+  uint8_t *kernel_ptr;
+
+  uint8_t *kmem;
+  uint8_t *aslr_patch;
+
+  uint16_t fw_version = args->kpayload_firmware_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, aslr_macro);
+
+  uint64_t cr0 = readCr0();
+  writeCr0(cr0 & ~X86_CR0_WP);
+
+  // This may change depending on new firmware's function structure
+  kmem = (uint8_t *)aslr_patch;
+  kmem[0] = 0xEB;
+  if (fw_version < 600 || fw_version > 755) {
+    // 3.50-5.56 and 8.00-9.60
+    kmem[1] = 0x00;
+  }
+
+  writeCr0(cr0);
+
+  return 0;
+}
+
+int kpayload_kernel_clock(struct thread *td, struct kpayload_kclock_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+
+  void (*sceSblSrtcSetTime)(uint64_t tm);
+  void (*sceSblSrtcClearTimeDifference)(uint64_t value);
+
+  uint16_t fw_version = args->kpayload_kclock_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, kclock_macro);
+
+  uint64_t set_time = args->kpayload_kclock_info->set_time;
+
+  sceSblSrtcSetTime(set_time);
+
+  return 0;
+}
+
+int kpayload_enable_browser(struct thread *td, struct kpayload_firmware_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+
+  uint64_t (*sceRegMgrSetInt)(uint32_t regId, int value);
+
+  uint16_t fw_version = args->kpayload_firmware_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, enable_browser_macro);
+
+  sceRegMgrSetInt(0x3C040000, 0);
+
+  return 0;
+}
+
+int kpayload_target_id(struct thread *td, struct kpayload_target_id_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+  uint8_t *kernel_ptr;
+
+  uint8_t *kmem;
+  uint8_t *tid_patch;
+
+  uint16_t fw_version = args->kpayload_target_id_info->fw_version;
+  uint8_t spoof = args->kpayload_target_id_info->spoof;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, tid_macro);
+
+  uint64_t cr0 = readCr0();
+  writeCr0(cr0 & ~X86_CR0_WP);
+
+  kmem = (uint8_t *)tid_patch;
+  kmem[0] = spoof;
+
+  writeCr0(cr0);
+
+  return 0;
+}
+
+int kpayload_perm_uart(struct thread *td, struct kpayload_firmware_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+
+  uint64_t (*icc_nvs_write)(uint32_t block, uint32_t offset, uint32_t size, void *value);
+
+  uint16_t fw_version = args->kpayload_firmware_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, icc_nvs_write_macro);
+
+  char uart = 1;
+  icc_nvs_write(4, 0x31F, 1, &uart);
+
+  return 0;
+}
+
+int kpayload_exit_idu(struct thread *td, struct kpayload_firmware_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+
+  uint64_t (*icc_nvs_write)(uint32_t block, uint32_t offset, uint32_t size, void *value);
+
+  uint16_t fw_version = args->kpayload_firmware_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, icc_nvs_write_macro);
+
+  char flag = 0;
+  icc_nvs_write(4, 0x1600, 1, &flag);
+
+  return 0;
+}
+
+int kpayload_npdrm_patch(struct thread *td, struct kpayload_firmware_args *args) {
+  UNUSED(td);
+  void *kernel_base;
+  uint8_t *kernel_ptr;
+
+  uint8_t *kmem;
+  uint8_t *npdrm_close;
+  uint8_t *npdrm_open;
+  uint8_t *npdrm_ioctl;
+
+  uint16_t fw_version = args->kpayload_firmware_info->fw_version;
+
+  // NOTE: This is a C preprocessor macro
+  build_kpayload(fw_version, npdrm_macro);
+
+  uint64_t cr0 = readCr0();
+  writeCr0(cr0 & ~X86_CR0_WP);
+
+  kmem = (uint8_t *)npdrm_open;
+  kmem[0] = 0x31;
+  kmem[1] = 0xC0;
+  kmem[2] = 0xC3;
+
+  kmem = (uint8_t *)npdrm_close;
+  kmem[0] = 0x31;
+  kmem[1] = 0xC0;
+  kmem[2] = 0xC3;
+
+  // This may change depending on new firmware's function structure
+  kmem = (uint8_t *)npdrm_ioctl;
+  kmem[0] = 0xEB;
+  if (fw_version < 500) {
+    // 3.50-4.74
+    kmem[1] = 0x04;
+  } else {
+    // 5.00-9.00
+    kmem[1] = 0x00;
+  }
+
+  writeCr0(cr0);
+
+  return 0;
+}
+
