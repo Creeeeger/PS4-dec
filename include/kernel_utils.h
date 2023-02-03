5c068b157862205f8fb32fc676e24ffec360ab29
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2023-02-03 14:37:25 +0000

    feat: define decryptor service interfaces

---
+++ include/kernel_utils.h
@@ -0,0 +1,53 @@
+#ifndef KERNEL_UTILS_H
+#define KERNEL_UTILS_H
+
+#include "fw_defines.h"
+#include "ps4.h"
+
+void decrypt_pups(const char * InputPath, const char * OutputPath);
+
+
+struct kpayload_get_fw_version_info {
+  uint64_t uaddr;
+};
+
+struct kpayload_get_fw_version_args {
+  void *syscall_handler;
+  struct kpayload_get_fw_version_info *kpayload_get_fw_version_info;
+};
+
+struct kpayload_jailbreak_info {
+  uint64_t fw_version;
+};
+
+struct kpayload_jailbreak_args {
+  void *syscall_handler;
+  struct kpayload_jailbreak_info *kpayload_jailbreak_info;
+};
+
+struct kpayload_get_kbase_info {
+  uint64_t fw_version;
+  uint64_t uaddr;
+};
+
+struct kpayload_get_kbase_args {
+  void *syscall_handler;
+  struct kpayload_get_kbase_info *kpayload_get_kbase_info;
+};
+
+struct kpayload_kernel_dumper_info {
+  uint64_t fw_version;
+  uint64_t uaddr;
+  uint64_t kaddr;
+  size_t size;
+};
+
+struct kpayload_kernel_dumper_args {
+  void *syscall_handler;
+  struct kpayload_kernel_dumper_info *kpayload_kernel_dumper_info;
+};
+
+uint64_t get_fw_version(void);
+int dump_kernel(uint64_t fw_version, uint64_t kaddr, uint64_t *dump, size_t size);
+
+#endif
