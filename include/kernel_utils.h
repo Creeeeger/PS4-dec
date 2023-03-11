3fcd7d9a98e77d0769b8af248fc2ae8ad8c0bf19
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2023-03-11 13:27:48 +0000

    chore: add SDK headers and build outputs
    Description: vendor libPS4 headers and generated artifacts.

--- include/kernel_utils.h
+++ include/kernel_utils.h
@@ -11,47 +11,6 @@
     systemMessage(message);                            \
   } while (0)
 
-struct auditinfo_addr {
-  char useless[184];
-};
-
-struct ucred {
-  uint32_t useless1;
-  uint32_t cr_uid;
-  uint32_t cr_ruid;
-  uint32_t useless2;
-  uint32_t useless3;
-  uint32_t cr_rgid;
-  uint32_t useless4;
-  void *useless5;
-  void *useless6;
-  void *cr_prison;
-  void *useless7;
-  uint32_t useless8;
-  void *useless9[2];
-  void *useless10;
-  struct auditinfo_addr useless11;
-  uint32_t *cr_groups;
-  uint32_t useless12;
-};
-
-struct filedesc {
-  void *useless1[3];
-  void *fd_rdir;
-  void *fd_jdir;
-};
-
-struct proc {
-  char useless[64];
-  struct ucred *p_ucred;
-  struct filedesc *p_fd;
-};
-
-struct thread {
-  void *useless;
-  struct proc *td_proc;
-};
-
 struct kpayload_get_fw_version_info {
   uint64_t uaddr;
 };
--- local.sh
+++ local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+make clean
 bash install.sh
 chmod +rwx Makefile
+export PS4SDK=/opt/ps4sdk
 make
 chmod +777 ps4_decryptor.bin
