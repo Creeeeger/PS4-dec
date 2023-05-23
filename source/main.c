6e10f0b40c1d7d6360dca7b98970ded6da504535
gregor <gregor@MacBook-Pro-von-crecker.local>
2023-05-23 10:55:24 +0100

    tests

--- source/main.c
+++ source/main.c
@@ -1,13 +1,9 @@
 #include "kernel_utils.h"
-//#include "decrypt.c"
-
 #define KERNEL_CHUNK_SIZE 0x1000
 #define KERNEL_CHUNK_NUMBER 0x69B8
 
 int sock;
-
 time_t prevtime;
-
 uint8_t GetElapsed(uint64_t ResetInterval) {
 
  time_t currenttime = time(0);
@@ -37,22 +33,18 @@ int _main(struct thread* td) {
   memset(server.sin_zero, 0, sizeof(server.sin_zero));
   sock = sceNetSocket("debug", AF_INET, SOCK_STREAM, 0);
   sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server));
-
   int flag = 1;
   sceNetSetsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(int));
 #endif
 
   uint64_t fw_version = get_fw_version();
   jailbreak(fw_version);
-  
-
   initSysUtil();
-
-    
   GetElapsed(0);
   printf_notification("Running PS4 PUP Decrypter");
   decrypt_pups("/mnt/usb0/safe.PS4UPDATE.PUP", "/mnt/usb0/%s.dec");
+  decrypt_pups();
+  decrypt_dir();
   printf_notification("Finished PS4 PUP Decrypter");
-
   return 0;
 }
