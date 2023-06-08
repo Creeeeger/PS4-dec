3fff042ec4638ae7370053547bc3db6892321b21
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2023-06-08 14:18:43 +0100

    build: create and compile the SDK workspace

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ else
     apt-get update
     apt-get -y install binutils gcc make
 fi
+
+# Delete directory if it exists and make empty directory
+if [ "$PWD" != "$(pwd)/ps4sdk" ]; then
+    rm -rf "$(pwd)/ps4sdk" || true
+    mkdir -p "$(pwd)/ps4sdk"
+fi
+
+# Build SDK
+cd libPS4 || (echo "Unable to enter subdirectory" && exit)
+make
+cd ..
